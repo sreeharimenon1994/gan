40f764637898181d824cffb1a2c326ae7064417d
Balu Reghunathan <braghunathan@trl.co.uk>
2020-04-02 14:05:56 +0100

    results

--- gan_me.py
+++ gan_me.py
@@ -1,10 +1,10 @@
 import os, time, itertools, pickle
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib.image import imsave
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
+tf.reset_default_graph()
 # G(z)
 def generator(x):
     # initializers
@@ -14,60 +14,60 @@ def generator(x):
     w0 = tf.get_variable('G_w0', [x.get_shape()[1], 256], initializer=w_init)
     b0 = tf.get_variable('G_b0', [256], initializer=b_init)
     h0 = tf.nn.leaky_relu(tf.matmul(x, w0) + b0)
-
-    ### Code:ToDo (Change the architecture as CW2 Guidance required)
-
-    w2 = tf.get_variable('G_w2', [h0.get_shape()[1], 512], initializer=w_init)
-    b2 = tf.get_variable('G_b2', [512], initializer=b_init)
-    h2 = tf.nn.leaky_relu(tf.matmul(h0, w2) + b2)
-
-    w3 = tf.get_variable('G_w3', [h2.get_shape()[1], 1024], initializer=w_init)
-    b3 = tf.get_variable('G_b3', [1024], initializer=b_init)
-    h3 = tf.nn.leaky_relu(tf.matmul(h2, w3) + b3)
-    
-    # output hidden layer
-    w1 = tf.get_variable('G_w1', [h3.get_shape()[1], 784], initializer=w_init)
-    b1 = tf.get_variable('G_b1', [784], initializer=b_init)
-    o = tf.nn.tanh(tf.matmul(h3, w1) + b1)
+     # 2nd hidden layer
+    w1 = tf.get_variable('G_w1', [h0.get_shape()[1], 512], initializer=w_init)
+    b1 = tf.get_variable('G_b1', [512], initializer=b_init)
+    h1 = tf.nn.leaky_relu(tf.matmul(h0, w1) + b1)
+     # 3rd hidden layer
+    w2 = tf.get_variable('G_w2', [h1.get_shape()[1], 1024], initializer=w_init)
+    b2 = tf.get_variable('G_b2', [1024], initializer=b_init)
+    h2 = tf.nn.leaky_relu(tf.matmul(h1, w2) + b2)
+      # output hidden layer
+    w3 = tf.get_variable('G_w3', [h2.get_shape()[1], 784], initializer=w_init)
+    b3 = tf.get_variable('G_b3', [784], initializer=b_init)
+    o = tf.nn.tanh(tf.matmul(h2, w3) + b3)
     return o
-
+    ### Code:ToDO( Change the architecture as CW2 Guidance required)
 
 # D(x)
 def discriminator(x, drop_out):
     # initializers
     w_init = tf.truncated_normal_initializer(mean=0, stddev=0.02)
     b_init = tf.constant_initializer(0.)
-    drop_out = 1 - drop_out
-    ###  Code: ToDO( Change the architecture as CW2 Guidance required)
     # 1st hidden layer
-
     w0 = tf.get_variable('D_w0', [x.get_shape()[1], 1024], initializer=w_init)
     b0 = tf.get_variable('D_b0', [1024], initializer=b_init)
     h0 = tf.nn.leaky_relu(tf.matmul(x, w0) + b0)
-    drop0 = tf.nn.dropout(h0, rate=drop_out)
-
-
-    w2 = tf.get_variable('D_w2', [drop0.get_shape()[1], 512], initializer=w_init)
-    b2 = tf.get_variable('D_b2', [512], initializer=b_init)
-    h2 = tf.nn.leaky_relu(tf.matmul(drop0, w2) + b2)
-    drop2 = tf.nn.dropout(h2, rate=drop_out)
-
-    w3 = tf.get_variable('D_w3', [drop2.get_shape()[1], 256], initializer=w_init)
-    b3 = tf.get_variable('D_b3', [256], initializer=b_init)
-    h3 = tf.nn.leaky_relu(tf.matmul(drop2, w3) + b3)
-    drop3 = tf.nn.dropout(h3, rate=drop_out)
-
+    dropout0 = tf.nn.dropout(x=h0,keep_prob=1-drop_out)
+    
+    # 2nd hidden layer
+    w1 = tf.get_variable('D_w1', [dropout0.get_shape()[1], 512], initializer=w_init)
+    b1 = tf.get_variable('D_b1', [512], initializer=b_init)
+    h1 = tf.nn.leaky_relu(tf.matmul(h0, w1) + b1)
+    dropout1 = tf.nn.dropout(x=h1,keep_prob=1-drop_out)
+    # 3rd hidden layer
+    w2 = tf.get_variable('D_w2', [dropout1.get_shape()[1], 256], initializer=w_init)
+    b2 = tf.get_variable('D_b2', [256], initializer=b_init)
+    h2 = tf.nn.leaky_relu(tf.matmul(h1, w2) + b2)
+    dropout2 = tf.nn.dropout(x=h2,keep_prob=1-drop_out)
     # output layer
-    w1 = tf.get_variable('D_w1', [drop3.get_shape()[1], 1], initializer=w_init)
-    b1 = tf.get_variable('D_b1', [1], initializer=b_init)
-    h1 = tf.sigmoid(tf.matmul(drop3, w1) + b1)
-    return h1
+    w3 = tf.get_variable('D_w3', [dropout2.get_shape()[1], 1], initializer=w_init)
+    b3 = tf.get_variable('D_b3', [1], initializer=b_init)
+    o = tf.sigmoid(tf.matmul(dropout2, w3) + b3)
+    ###  Code: ToDO( Change the architecture as CW2 Guidance required)
+
+    return o
 
 
-def show_result(num_epoch, show = False, save = False, path = 'result.png'):
+
+#def show_result(num_epoch, show = False, save = False, path = 'C:\\Users\\Femi\\Desktop\\aworkspace\\Practice\\GAN_Practice\\GAN\\result.png'):
+def show_result(num_epoch, show = False, save = False, path = '.\\GAN\\result.png'):
     z_ = np.random.normal(0, 1, (25, 100))    # z_ is the input of generator, every epochs will random produce input
     ##Code:ToDo complete the rest of part
+
+    images = []
     gen_images = sess.run(G_z, {z: z_})
+    images.append(gen_images)
    
     gen_images = gen_images.reshape(25,28,28)
     
@@ -84,10 +84,12 @@ def show_result(num_epoch, show = False, save = False, path = 'result.png'):
     if show:
         plt.show()
     else:
-        plt.close()
+        plt.close()   
+     
+   
+#def show_train_hist(hist, show = False, save = False, path = 'C:\\Users\\Femi\\Desktop\\aworkspace\\Practice\\GAN_Practice\\GAN\\Train_hist.png'):
+def show_train_hist(hist, show = False, save = False, path = '.\\GAN\\Train_hist.png'):
 
-
-def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
     x = range(len(hist['D_losses']))
 
     y1 = hist['D_losses']
@@ -111,13 +113,11 @@ def show_train_hist(hist, show = False, save = False, path = 'Train_hist.png'):
     else:
         plt.close()
 
-
 # training parameters
 batch_size = 100
 lr = 0.0002
 train_epoch = 100
-
-tf.reset_default_graph()
+drop_out = 0.3
 
 # load MNIST
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
@@ -157,7 +157,7 @@ tf.global_variables_initializer().run()
 if not os.path.isdir('MNIST_GAN_results'):
     os.mkdir('MNIST_GAN_results')
 if not os.path.isdir('MNIST_GAN_results/results'):
-    os.mkdir('MNIST_GAN_sree_results/results')
+    os.mkdir('MNIST_GAN_results/results')
 train_hist = {}
 train_hist['D_losses'] = []
 train_hist['G_losses'] = []
@@ -188,9 +188,10 @@ for epoch in range(train_epoch):
     print('[%d/%d] - ptime: %.2f loss_d: %.3f, loss_g: %.3f' % ((epoch + 1), train_epoch, per_epoch_ptime, np.mean(D_losses), np.mean(G_losses)))
 
     ### Code: TODO Code complete show_result function)
-    p = 'MNIST_GAN_sree_results/results/MNIST_GAN_' + str(epoch + 1) + '.png'
-    show_result((epoch + 1), save=True, path=p, show=False)
-
+    p = 'MNIST_GAN_results/results/MNIST_GAN_' + str(epoch + 1) + '.png'
+    show_result((epoch + 1), save=True, path=p)
+    
+    
     train_hist['D_losses'].append(np.mean(D_losses))
     train_hist['G_losses'].append(np.mean(G_losses))
     train_hist['per_epoch_ptimes'].append(per_epoch_ptime)
@@ -199,8 +200,8 @@ total_ptime = end_time - start_time
 train_hist['total_ptime'].append(total_ptime)
 print('Avg per epoch ptime: %.2f, total %d epochs ptime: %.2f' % (np.mean(train_hist['per_epoch_ptimes']), train_epoch, total_ptime))
 print("Training finish!... save training results")
-with open('MNIST_GAN_sree_results/train_hist.pkl', 'wb') as f:
+with open('MNIST_GAN_results/train_hist.pkl', 'wb') as f:
     pickle.dump(train_hist, f)
-show_train_hist(train_hist, save=True, path='MNIST_GAN_sree_results/MNIST_GAN_train_hist.png')
-images = []
+show_train_hist(train_hist, save=True, path='MNIST_GAN_results/MNIST_GAN_train_hist.png')
+#images = []
 sess.close()
